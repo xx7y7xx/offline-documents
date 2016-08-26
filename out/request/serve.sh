ce467b82d9ce09dd74376d26b2eefc9aff6175c2
Devin Chen <d3vin.chen@gmail.com>
2016-08-26 10:30:54 +0800

    update

---
+++ out/request/serve.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://gist.github.com/pwittchen/531c0bed8a20c1f06231
+# starting simple HTTP server with Python in background
+screen -d -m python -m SimpleHTTPServer 9003
--- serve.sh
+++ serve.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-python -m SimpleHTTPServer
+cd out
+
+for dir in */
+do
+  cd $dir
+  if [ -f serve.sh ]; then
+    ./serve.sh
+  fi
+  cd ..
+done
+
+cd ..
